c2eded475893f9a085f82381b6ba871394cfe9e1
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2026-03-06 20:35:54 -0800

    restored from main

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 import keycloak from '../keycloak';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL
-    ? `${import.meta.env.VITE_BACKEND_URL}/api`
-    : 'http://localhost:5050/api',
+  baseURL: '/api',
 });
 
 api.interceptors.request.use(async (config) => {
