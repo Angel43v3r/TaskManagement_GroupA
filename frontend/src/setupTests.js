448eefbdd5db9fcd3eeae3d01932db29beb1ab80
Yuria Loo <yuria.loo@comcast.net>
2026-03-23 16:24:32 -0700

    add a func to ignore messages

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -5,3 +5,27 @@ globalThis.React = React;
 import * as matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 expect.extend(matchers);
+
+const originalError = console.error;
+
+beforeAll(() => {
+  console.error = (...args) => {
+    const message = args[0];
+
+    if (
+      typeof message === 'string' &&
+      (message.includes('not wrapped in act') ||
+        message.includes('TransitionGroup') ||
+        message.includes('anchorEl') ||
+        message.includes('ForwardRef'))
+    ) {
+      return; // ignore these warnings
+    }
+
+    originalError(...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
