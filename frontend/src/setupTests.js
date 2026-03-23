ce380baf8453894aff4e0ea2c958294c4afb9ed2
Yuria Loo <yuria.loo@comcast.net>
2026-03-23 15:50:44 -0700

    fix lint

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,6 +1,6 @@
 console.log('🚀 SETUP FILE IS RUNNING');
 import React from 'react';
-global.React = React;
+globalThis.React = React;
 
 import * as matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
