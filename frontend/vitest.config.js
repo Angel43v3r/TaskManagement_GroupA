1a41fdc8a0a0e3a11fd76417afe868710b4c48aa
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2026-03-22 19:36:55 -0700

    Let's see if this works

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    root: './frontend',
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    include: ['**/*.test.{js,jsx}'],
+    setupFiles: ['./frontend/src/setupTests.js'],
+    include: ['./frontend/src/tests/**/*.test.{js,jsx}'],
+    globals: true,
   },
 });
