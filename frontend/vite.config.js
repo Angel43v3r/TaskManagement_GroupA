3288614f59b0ac0f896b69808c43768f53d041c7
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2026-03-23 18:42:39 -0700

    update from main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig({
   // https://vite.dev/config/
-  return defineConfig({
-    server: {
-      port: 3000,
-      host: true,
-      proxy: {
-        '/api': {
-          target: 'http://backend:5050',
-          changeOrigin: true,
-          secure: false,
-        },
+  server: {
+    port: 3000,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:5050',
+        changeOrigin: true,
+        secure: false,
       },
     },
-    plugins: [react()],
-    test: {
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.js',
-    },
-  });
-};
+  },
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+});
